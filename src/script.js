596a96b71be2268d872f2b3d84e3bba1c28507a5
TheDoomfire <90956505+TheDoomfire@users.noreply.github.com>
2022-05-23 16:54:40 +0200

    Added some clamps
    And made separate files for list and tablets.

--- src/script.js
+++ src/script.js
@@ -2,18 +2,22 @@
 // https://www.youtube.com/watch?v=flItyHiDm7E&t=22s
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".main-nav");
-const header = document.querySelector(".main-header");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    header.classList.toggle("active");
 });
 
 document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+// Trying to get the aria-expanded to toggle.
+/* let el = document.getElementById('hamburger');
+console.log(el.ariaExpanded); // false
+el.ariaExpanded = "false";
+console.log(el.ariaExpanded); // true */
 
 
 //Trying to store current stock price.
